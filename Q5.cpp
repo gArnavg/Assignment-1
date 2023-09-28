b2483307b1cdf3aa6b905d6daea7a7176a82375f
gArnavg <117974343+gArnavg@users.noreply.github.com>
2023-09-28 17:22:40 +0530

    Programs Added

---
+++ Q5.cpp
@@ -0,0 +1,11 @@
+/*Write a program to find the circumference of a circle with 
+radius 10 in C++.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r = 10;
+    float circumference = 6.28*r;
+    cout<<"Thus the Circumference is "<<circumference<<" Sqr Units";
+    return 0;
+}
